1669e0abe28311da06ba07797df96953b4beb0a5
pdrotmz <pedrotomaz0210@gmail.com>
2025-04-16 15:52:57 -0300

    feat: adicionar route
    - Adiciona route de layout a "/"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [
     {
         path: "",
-        component: HeaderComponent
+        component: LayoutComponent,
     }
 ];
