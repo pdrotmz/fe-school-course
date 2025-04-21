e2d536726e2f395f23872360738f884c9ff97729
pdrotmz <pedrotomaz0210@gmail.com>
2025-04-21 20:27:27 -0300

    feat: Adiciona layour de course-card
    - Chama course-card pra dentro do layout
    - Importa o componente do layout.component.ts

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from '../course-card/course-card.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CourseCardComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
